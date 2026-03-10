4622e9b81b450b1e77e8738fb5c058dd0b7d2ddc
Jason Haruska <2803+haruska@users.noreply.github.com>
2026-03-10 17:44:39 -0500

    add label and litestream setup

--- Dockerfile
+++ Dockerfile
@@ -74,6 +74,9 @@ RUN SECRET_KEY_BASE_DUMMY=1 ./bin/rails assets:precompile
 # Final stage for app image
 FROM base
 
+# For kamal deployments
+LABEL service="bracket-lab"
+
 # Run and own only the runtime files as a non-root user for security
 RUN groupadd --system --gid 1000 rails && \
     useradd rails --uid 1000 --gid 1000 --create-home --shell /bin/bash
--- config/puma.rb
+++ config/puma.rb
@@ -33,8 +33,11 @@ port ENV.fetch("PORT", 3000)
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-  # Run the Solid Queue supervisor inside of Puma for single-server deployments
-  plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+# Run the Solid Queue supervisor inside of Puma for single-server deployments
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+
+# enable litestream backups if bucket is configured
+plugin :litestream if ENV["LITESTREAM_REPLICA_BUCKET"].present?
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
