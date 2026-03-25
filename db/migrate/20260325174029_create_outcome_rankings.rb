396f12284cd035af76268edb5483bcddd1318089
Jason Haruska <2803+haruska@users.noreply.github.com>
2026-03-25 14:50:52 -0500

    Consolidate into single migration for outcome_rankings

--- db/migrate/20260325174029_create_outcome_rankings.rb
+++ db/migrate/20260325174029_create_outcome_rankings.rb
@@ -1,15 +1,11 @@
-class CreateOutcomes < ActiveRecord::Migration[8.1]
+class CreateOutcomeRankings < ActiveRecord::Migration[8.1]
   def change
-    create_table :outcomes do |t|
-      t.bigint :game_decisions, null: false
-      t.index :game_decisions, unique: true
-    end
-
     create_table :outcome_rankings do |t|
-      t.references :outcome, null: false, foreign_key: { on_delete: :cascade }
+      t.bigint :game_decisions, null: false
       t.references :bracket, null: false, foreign_key: true
       t.integer :rank, null: false
       t.integer :points, null: false
+      t.index :game_decisions
       t.index [ :bracket_id, :rank ]
     end
 
