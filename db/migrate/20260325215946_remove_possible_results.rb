cc44f13fccab9eda4ab33150084aa7ef795d58b9
Jason Haruska <2803+haruska@users.noreply.github.com>
2026-03-25 17:00:32 -0500

    Add timestamps to outcome_rankings and drop possible_results table

---
+++ db/migrate/20260325215946_remove_possible_results.rb
@@ -0,0 +1,12 @@
+class RemovePossibleResults < ActiveRecord::Migration[8.1]
+  def change
+    drop_table :possible_results do |t|
+      t.integer "best_finish", default: 1, null: false
+      t.integer "bracket_id", null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["bracket_id"], name: "index_possible_results_on_bracket_id", unique: true
+      t.index ["updated_at"], name: "index_possible_results_on_updated_at"
+    end
+  end
+end
